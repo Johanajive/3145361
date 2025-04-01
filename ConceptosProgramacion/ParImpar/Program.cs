b75a7b86eabdd90930d9ea76f8681b20fc102913
JOHANA JIMENEZ <JOHANAJIME233@GMAIL.COM>
2025-03-31 23:52:16 -0500

    Número Par e Impar

--- ConceptosProgramacion/ParImpar/Program.cs
+++ ConceptosProgramacion/ParImpar/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var i = 1;
+do {
+Console.Write($"Ingrese el número: {i} un número Entero:");
+var numString = (Console.ReadLine());
+
+    if (int.TryParse(numString, out int numInt))
+    {
+        if (numInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numInt} es Par");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numInt} es Impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine("El valor '" + numString + "' no es un numero Entero");
+    }
+    i++; 
+}while (i <= 3);
